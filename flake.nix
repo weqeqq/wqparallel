869c15b89a7d0ab896d78f2eaa16cca078dcc6e5
weqeqq <ggmodwear@gmail.com>
2026-04-03 15:54:48 +0300

    chore(release): bump version to 0.2.1

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
           mkLibrary = { tests ? false }:
             pkgs.stdenv.mkDerivation {
               pname = "wqparallel";
-              version = "0.2.0";
+              version = "0.2.1";
               src = ./.;
 
               strictDeps = true;
